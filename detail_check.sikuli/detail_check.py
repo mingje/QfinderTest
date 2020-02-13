8a20abd1acd1be1da04137e69cb0d0a24d90c00c
stevenhsu <mingje1104@gmail.com>
2020-02-13 11:39:41 +0800

    cccv

--- detail_check.sikuli/detail_check.py
+++ detail_check.sikuli/detail_check.py
@@ -4,7 +4,7 @@ Settings.OcrTextSearch = True
 Settings.OcrTextRead = True
 
 import sys
-"""
+
 nas_name = sys.argv[1]
 nas_lanip1 = sys.argv[2]
 nas_ac = sys.argv[3]
@@ -14,7 +14,7 @@ nas_name = "Jack-TS932X"
 nas_lanip1 = "10.20.240.109"
 nas_ac = "admin"
 nas_pwd = "jack2030"
-
+"""
 target = nas_detail(name = nas_name, lanip1 = nas_lanip1, ac = nas_ac, pwd = nas_pwd)
 print(target)
 
