c94da57f0a65a26892876dbd2bc598ab42e60fbc
stevenhsu <mingje1104@gmail.com>
2020-03-16 17:53:50 +0800

    change nas info

--- nas_info.sikuli/nas_info.py
+++ nas_info.sikuli/nas_info.py
@@ -11,22 +11,33 @@ path = delimiter.join(current_path1) + "\\screenshot\\"
 def nas_detail(**kwargs):
     nas = {}
     nas["name"] = kwargs.get("name")
-    nas["icon"] = path + kwargs.get("name") + "\\" + kwargs.get("name") + ".png"
-    nas["icon_1"] = path + kwargs.get("name") + "\\" + kwargs.get("name") + "_1.png"
-    nas["icon_highlight"] = path + kwargs.get("name") + "\\" + kwargs.get("name") + "_highlight.png"
-    nas["icon_g"] = path + kwargs.get("name") + "\\" + kwargs.get("name") + "_g.png"
-    nas["icon_gh"] = path + kwargs.get("name") + "\\" + kwargs.get("name") + "_gh.png"    
+    try:
+        check_page = path + kwargs.get("name") + "\\detail_check\\"
+        detail_list = os.listdir(check_page)
+        nas["icon"] = path + kwargs.get("name") + "\\" + kwargs.get("name") + ".png"
+        nas["icon_1"] = path + kwargs.get("name") + "\\" + kwargs.get("name") + "_1.png"
+        nas["icon_highlight"] = path + kwargs.get("name") + "\\" + kwargs.get("name") + "_highlight.png"
+        nas["icon_g"] = path + kwargs.get("name") + "\\" + kwargs.get("name") + "_g.png"
+        nas["icon_gh"] = path + kwargs.get("name") + "\\" + kwargs.get("name") + "_gh.png"    
+    except:
+        default_path = "Jack-TS932X"
+        check_page = path + default_path + "\\detail_check\\"
+        detail_list = os.listdir(check_page)
+        nas["icon"] = path + default_path + "\\" + default_path + ".png"
+        nas["icon_1"] = path + default_path + "\\" + default_path + "_1.png"
+        nas["icon_highlight"] = path + default_path + "\\" + default_path + "_highlight.png"
+        nas["icon_g"] = path + default_path + "\\" + default_path + "_g.png"
+        nas["icon_gh"] = path + default_path + "\\" + default_path + "_gh.png"    
     nas["lanip1"] = kwargs.get("lanip1")   
     nas["lanip1P"] = path + "xx.png"
     nas["lanip2"] = kwargs.get("lanip2")
     nas["ac"] = kwargs.get("ac")
     nas["pwd"] = kwargs.get("pwd")
     nas["qid"] = kwargs.get("qid")
-    check_page = path + kwargs.get("name") + "\\detail_check\\"
+    # check_page = path + kwargs.get("name") + "\\detail_check\\"
     detail_list = os.listdir(check_page)
     check_list = []
     for i in detail_list:
         check_list.append(check_page + i)
     nas["detail_check"] = check_list
     return nas
-
